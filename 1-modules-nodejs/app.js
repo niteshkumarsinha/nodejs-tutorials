3be9ad74883e4b8a4a4b0a6f9142127b26e1cc9e
niteshkumar <niteshkumar@niteshkumars-MacBook-Pro.local>
2025-12-29 17:59:46 +0530

    nodejs tutorials

--- 1-modules-nodejs/app.js
+++ 1-modules-nodejs/app.js
@@ -1,6 +1,10 @@
 const math = require("./math");
 
-console.log(math.add(5, 10));
-console.log(math.sub(5, 10));
-console.log(math.mul(5, 10));
-console.log(math.div(5, 10));
+const { add, sub, mul, div, PI } = math;
+
+console.log(add(5, 10));
+console.log(sub(5, 10));
+console.log(mul(5, 10));
+console.log(div(5, 10));
+console.log(PI);
+
--- 1-modules-nodejs/math.js
+++ 1-modules-nodejs/math.js
@@ -2,5 +2,6 @@ const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
+const PI = 3.14;
 
-module.exports = { add, sub, mul, div };
+module.exports = { add, sub, mul, div, PI };
